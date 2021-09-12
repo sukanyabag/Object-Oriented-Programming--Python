9edac23378c6e5e0879f63944dcc52b87d981b2e
Sukanya Bag <sukannya472@gmail.com>
2021-09-12 19:49:54 +0530

    Create operatorOverload.py

---
+++ operatorOverload.py
@@ -0,0 +1,15 @@
+# Python program to show use of
+# + operator for different purposes.
+ 
+print(1 + 2)
+ 
+# concatenate two strings
+print("Wanda"+"Vision")
+ 
+# Product two numbers
+print(3 * 4)
+ 
+# Repeat the String
+print("LOL"*4)
+
+
