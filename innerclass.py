66a32f2dc8349d74de3f0ad816deca26931b7f13
Sukanya Bag <sukannya472@gmail.com>
2021-09-12 19:36:40 +0530

    Create innerclass.py

---
+++ innerclass.py
@@ -0,0 +1,35 @@
+#inner class => nested class
+# create a Color class
+class Color:
+    
+  # constructor method
+  def __init__(self):
+    # object attributes
+    self.name = 'Green'
+    self.lg = self.Lightgreen()
+    
+  def show(self):
+    print("Name:", self.name)
+    
+  # create Lightgreen class
+  class Lightgreen:
+     def __init__(self):
+        self.name = 'Light Green'
+        self.code = '024avc'
+    
+     def display(self):
+        print("Name:", self.name)
+        print("Code:", self.code)
+  
+# create Color class object
+outer = Color()
+  
+# method calling
+outer.show()
+  
+# create a Lightgreen 
+# inner class object
+g = outer.lg
+  
+# inner class method calling
+g.display()
