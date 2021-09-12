641e271d9b493fe167a5c465f7999f3380448723
Sukanya Bag <sukannya472@gmail.com>
2021-09-12 19:46:10 +0530

    Create duckTyping.py

---
+++ duckTyping.py
@@ -0,0 +1,27 @@
+# Python program to demonstrate
+# duck typing
+  
+  
+class Bird:
+    def fly(self):
+        print("fly with wings")
+  
+class Airplane:
+    def fly(self):
+        print("fly with fuel")
+  
+class Fish:
+    def swim(self):
+        print("fish swim in sea")
+  
+# Attributes having same name are
+# considered as duck typing
+for obj in Bird(), Airplane(), Fish():
+    obj.fly()
+    
+'''
+Output:
+
+fly with wings
+fly with fuel
+'''
