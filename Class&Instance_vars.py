f1486bcab80edeb784dfbe123abed7482c7c1832
Sukanya Bag <sukannya472@gmail.com>
2021-09-12 19:32:38 +0530

    Create Class&Instance_vars.py

---
+++ Class&Instance_vars.py
@@ -0,0 +1,31 @@
+# Class for Dog
+class Dog:
+   
+    # Class Variable
+    animal = 'dog'            
+   
+    # The init method or constructor
+    def __init__(self, breed, color):
+     
+        # Instance Variable    
+        self.breed = breed
+        self.color = color       
+    
+# Objects of Dog class
+Rodger = Dog("Pug", "brown")
+Buzo = Dog("Bulldog", "black")
+ 
+print('Rodger details:')  
+print('Rodger is a', Rodger.animal)
+print('Breed: ', Rodger.breed)
+print('Color: ', Rodger.color)
+ 
+print('\nBuzo details:')  
+print('Buzo is a', Buzo.animal)
+print('Breed: ', Buzo.breed)
+print('Color: ', Buzo.color)
+ 
+# Class variables can be accessed using class
+# name also
+print("\nAccessing class variable using class name")
+print(Dog.animal)    
